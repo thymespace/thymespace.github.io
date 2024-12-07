2ffc2d454da24748ab826888a1ed058ebadcb1e4
thymespace <puffstroom@gmail.com>
2024-12-07 10:14:07 +0800

    Inner + Localisation

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const Localisation = [
+
+    "IntroductionHeader",
+    "About me",
+    "ＡＢＯＵＴ",
+
+    "IntroductionBody",
+    "",
+    "日本語まだ勉強中なので、間違いがあったら許してください。\n"
+
+];
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -8,16 +20,22 @@ function LanguageSelectAnimation(languageButton){
 
     var MainContainer = document.getElementById("MainContainer")
 
-    MainContainer.style.top      = "calc("           + buttonRect.top    + "px + 1.5vw)";
-    MainContainer.style.bottom   = "calc( 100vh - "  + buttonRect.bottom + "px + 1.5vw)";
-    MainContainer.style.right    = "calc( 100vw - "  + buttonRect.right  + "px + 1.5vw)";
-    MainContainer.style.left     = "calc("           + buttonRect.left   + "px + 1.5vw)";
+    MainContainer.style.top      = "calc("           + buttonRect.top    + "px)";
+    MainContainer.style.bottom   = "calc( 100vh - "  + buttonRect.bottom + "px)";
+    MainContainer.style.right    = "calc( 100vw - "  + buttonRect.right  + "px)";
+    MainContainer.style.left     = "calc("           + buttonRect.left   + "px)";
     
     //document.getElementById("MainContainer").style.transitionDuration = "1s";
     MainContainer.style.opacity = 100;
     MainContainer.style.animation = "MainContainerOpen";
     MainContainer.style.animationDuration = "1s";
     MainContainer.style.animationFillMode = "forwards"
+
+    MainContainer.style.pointerEvents = "all";
+
+    MainContainer.style.setProperty("--MainContainerStartTop", MainContainer.style.top);
+    MainContainer.style.setProperty("--MainContainerStartBottom", MainContainer.style.bottom);
+
 }
 
 function TransitionToMain(){
@@ -27,11 +45,14 @@ function TransitionToMain(){
     document.getElementById("ButtonContainer").style.opacity = 0;
     document.getElementById("ButtonContainer").style.pointerEvents = "none";
     document.getElementById("Sea").style.top = "80vh";
+
+    document.getElementById("Audio_Ambient").loop = true;
+    document.getElementById("Audio_Ambient").play();
 }
 
 function LanguageSelect_EN(){
 
-    SetEnglish();
+    SetLanguage(0);
     LanguageSelectAnimation(document.getElementById("LanguageEN"));
     TransitionToMain();
 
@@ -39,20 +60,16 @@ function LanguageSelect_EN(){
 
 function LanguageSelect_JP(){
 
-    SetJapanese();
+    SetLanguage(1);
     LanguageSelectAnimation(document.getElementById("LanguageJP"));
     TransitionToMain();
 
 }
 
-function SetEnglish(){
+function SetLanguage(language){
 
-    document.getElementById("temp").innerText = "Placeholder text";
-
-}
-
-function SetJapanese(){
-
-    document.getElementById("temp").innerText = "仮";
+    for(let i = 0; i <Localisation.length; i += 3){
+        document.getElementById(Localisation[i]).innerText = Localisation[i + 1 + language];
+    }
 
 }
